5b446effb9ba06962e987aca75614a8166be48fc
ManliestBen <benjamintmanley@gmail.com>
2020-07-04 10:36:32 -0500

    movie/tvshow card style complete

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -4,17 +4,22 @@ import { Link } from 'react-router-dom';
 function MovieCard(props) {
     return(
         <>
-            <div className="row">
-                <div className="col s12 m6">
-                <div className="card">
-                    <div className="card-image">
-                    <img src={props.movie.image ? props.movie.image : "https://www.cebodtelecom.com/wp-content/uploads/2014/09/related_post_no_available_image.png"} onClick={()=> {}}/>
-                    </div>
-                    <div className="card-content">
-                    <span className="card-title">{props.movie.name}</span>
-                    <p>{props.movie.description}</p>
-                    </div>
+            <div className=" card">
+                <div className="card-image waves-effect waves-block waves-light">
+                <img className="activator" src={props.movie.image ? props.movie.image : "https://www.cebodtelecom.com/wp-content/uploads/2014/09/related_post_no_available_image.png"} onClick={()=> {}}/>
                 </div>
+                <div className="card-content">
+                <span className="card-title activator grey-text text-darken-4">{props.movie.name}<i className="material-icons right">more_vert</i></span>
+                <p>{props.movie.description}</p>
+                </div>
+                <div className="card-reveal">
+                <span className="card-title grey-text text-darken-4">{props.movie.name}<i class="material-icons right">close</i></span>
+                <h6>IMDB Rating: {props.movie.imdbRating}</h6>
+                <div>Genre:  {props.movie.genre}</div>
+                <div>Release Year:  {props.movie.releaseDate}</div>
+                <div>Cast: {props.movie.cast.join(', ')}</div>
+                <div>MPAA Rating:  {props.movie.mpaaRating}</div>
+                <p>{props.movie.description}</p>
                 </div>
             </div>
         </>
