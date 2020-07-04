0a587b208ef49d77c976837387aaf00f59c07842
ManliestBen <benjamintmanley@gmail.com>
2020-07-03 22:30:01 -0500

    add index functions for API

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -5,9 +5,9 @@ const NavBar = () => {
     return (
     <>
         <nav>
-            <div class="nav-wrapper grey">
-                <a class="left brand-log" href="/"><img src="https://fontmeme.com/permalink/200704/d43fd863a6d3e281f9704c12d58a105c.png" height='66' alt="netflix-font" border="0"/></a>
-                <ul id="nav-mobile" class="right">
+            <div className="nav-wrapper grey">
+                <a className="left brand-log" href="/"><img src="https://fontmeme.com/permalink/200704/d43fd863a6d3e281f9704c12d58a105c.png" height='66' alt="netflix-font" border="0"/></a>
+                <ul id="nav-mobile" className="right">
                     <li><a href="/movies">All Movies</a></li>
                     <li><a href="/movies/add">Add a Movie</a></li>
                     <li><a href="/tvshows">TV Shows</a></li>
--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,16 +1,29 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom'
-import logo from './logo.svg';
 import './App.css';
 import NavBar from '../../components/NavBar/NavBar';
 import AddMoviePage from '../AddMoviePage/AddMoviePage';
 import AddTVShowPage from '../AddTVShowPage/AddTVShowPage';
 import MovieListPage from '../MovieListPage/MovieListPage';
 import TVShowListPage from '../TVShowListPage/TVShowListPage';
+import * as movieAPI from '../../services/movies-api';
+import * as TVShowAPI from '../../services/tvshows-api';
 
 
 class App extends Component {
-  
+  state = {
+    movies: [],
+    tvshows: []
+  }
+
+
+  async componentDidMount() {
+    const movies = await movieAPI.getAll();
+    this.setState({movies})
+    const tvshows = await TVShowAPI.getAll();
+    this.setState({tvshows}) 
+  }
+
   render () {
     return (
       <>
