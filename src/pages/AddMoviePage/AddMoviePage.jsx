e4b47ea81d44235858680da04a74ab631276bf2b
ManliestBen <benjamintmanley@gmail.com>
2020-07-04 15:50:28 -0500

    clean up movie add form

--- src/pages/AddMoviePage/AddMoviePage.jsx
+++ src/pages/AddMoviePage/AddMoviePage.jsx
@@ -8,10 +8,10 @@ class AddMoviePage extends Component {
             cast: [],
             description: '',
             mpaaRating: '',
-            releaseDate: 0,
-            runTime: 0,
+            releaseDate: '',
+            runTime: '',
             genre: '',
-            imdbRating: 0,
+            imdbRating: '',
             image: ''
         }
     };
@@ -31,26 +31,64 @@ class AddMoviePage extends Component {
         });
     };
 
+
     render() {
         return (
             <>
                 <div className="row">
                     <form className="col s12" ref={this.formRef} onSubmit={this.handleSubmit}>
                         <div className="row">
-                            <div className="input-field col s6">Movie Name:
-                            <input name="name" id="first_name" type="text" class="active" value={this.state.formData.name} onChange={this.handleChange} required />
+                            <div className="input-field col s6">
+                            <input name="name" id="movie_name" type="text" class="active" value={this.state.formData.name} onChange={this.handleChange} required />
+                            <label for="movie_name">Movie Name</label>
                             </div>
                         </div>
                         <div className="row">
-                            <div className="input-field col s6">Cast (Separate with commas):
-                            <input name="cast" id="first_name" type="text" class="active" value={this.state.formData.cast} onChange={this.handleChange} required/>
+                            <div className="input-field col s6">
+                            <input name="cast" id="cast" type="text" class="active" value={this.state.formData.cast} onChange={this.handleChange} required/>
+                            <label for="cast">Cast (Separate with commas)</label>
                             </div>
                         </div>
                         <div className="row">
-                            <div className="input-field col s6">Description:
-                            <input name="description" id="first_name" type="text" class="active" value={this.state.formData.description} onChange={this.handleChange}/>
+                            <div className="input-field col s6">
+                            <input name="description" id="description" type="text" class="active" value={this.state.formData.description} onChange={this.handleChange}/>
+                            <label for="description">Description</label>
                             </div>
                         </div>
+                        <div><label>MPAA Rating</label>
+                            <p>
+                                <label>  
+                                    <input name="mpaaRating" value="G" onChange={this.handleChange} type="radio"/>
+                                    <span>G</span>
+                                </label>
+                            </p>
+                            <p>
+                                <label>  
+                                    <input name="mpaaRating" value="PG" onChange={this.handleChange} type="radio"/>
+                                    <span>PG</span>
+                                </label>
+                            </p>
+                            <p>
+                                <label>  
+                                    <input name="mpaaRating" value="PG-13" onChange={this.handleChange} type="radio"/>
+                                    <span>PG-13</span>
+                                </label>
+                            </p>
+                            <p>
+                                <label>  
+                                    <input name="mpaaRating" value="R" onChange={this.handleChange} type="radio"/>
+                                    <span>R</span>
+                                </label>
+                            </p>
+                            <p>
+                                <label>  
+                                    <input name="mpaaRating" value="NC-17" onChange={this.handleChange} type="radio"/>
+                                    <span>NC-17</span>
+                                </label>
+                            </p>
+                        </div>
+
+{/* 
                         <label>MPAA Rating</label>
                         <select name="mpaaRating" className="browser-default col s12" value={this.state.formData.mpaaRating} onChange={this.handleChange}>
                             <option value="" disabled selected>Please select</option>
@@ -59,30 +97,35 @@ class AddMoviePage extends Component {
                             <option value="PG-13">PG-13</option>
                             <option value="R">R</option>
                             <option value="NC-17">NC-17</option>
-                        </select>
+                        </select> */}
                         <div className="row">
-                            <div className="input-field col s6">Release Year:
-                            <input name="releaseDate" id="first_name" type="text" class="active" value={this.state.formData.releaseDate} onChange={this.handleChange}/>
+                            <div className="input-field col s6">
+                            <input name="releaseDate" id="release" type="text" class="active" value={this.state.formData.releaseDate} onChange={this.handleChange}/>
+                            <label for="release">Release Year</label>
                             </div>
                         </div>
                         <div className="row">
-                            <div className="input-field col s6">Run-time (Min):
-                            <input name="runTime" id="first_name" type="text" class="active" value={this.state.formData.runTime} onChange={this.handleChange}/>
+                            <div className="input-field col s6">
+                            <input name="runTime" id="runtime" type="text" class="active" value={this.state.formData.runTime} onChange={this.handleChange}/>
+                            <label for="runtime">Run-time (Min)</label>
                             </div>
                         </div>
                         <div className="row">
-                            <div className="input-field col s6">Genre:
-                            <input name="genre" id="first_name" type="text" class="active" value={this.state.formData.genre} onChange={this.handleChange}/>
+                            <div className="input-field col s6">
+                            <input name="genre" id="genre" type="text" class="active" value={this.state.formData.genre} onChange={this.handleChange}/>
+                            <label for="genre">Genre</label>
                             </div>
                         </div>
                         <div className="row">
-                            <div className="input-field col s6">IMDB Rating:
-                            <input name="imdbRating" id="first_name" type="text" class="active" value={this.state.formData.imdbRating} onChange={this.handleChange}/>
+                            <div className="input-field col s6">
+                            <input name="imdbRating" id="imdb" type="text" class="active" value={this.state.formData.imdbRating} onChange={this.handleChange}/>
+                            <label for="imdb">IMDB Rating</label>
                             </div>
                         </div>
                         <div className="row">
-                            <div className="input-field col s6">Image URL:
-                            <input name="image" id="first_name" type="text" class="active" value={this.state.formData.image} onChange={this.handleChange}/>
+                            <div className="input-field col s6">
+                            <input name="image" id="imageURL" type="text" class="active" value={this.state.formData.image} onChange={this.handleChange}/>
+                            <label for="imageURL">Image URL</label>
                             </div>
                         </div>
                         <button
