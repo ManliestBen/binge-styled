46832a56fa0d08a209497abaab3b5c026d695d8b
ManliestBen <benjamintmanley@gmail.com>
2020-07-04 10:26:25 -0500

    adjust styling for tv card

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -5,12 +5,12 @@ const NavBar = () => {
     return (
     <>
         <nav>
-            <div className="nav-wrapper grey">
+            <div className="nav-wrapper">
                 <a className="left brand-logo" href="/"><img src="https://fontmeme.com/permalink/200704/d43fd863a6d3e281f9704c12d58a105c.png" height='66' alt="netflix-font" border="0"/></a>
                 <ul id="nav-mobile" className="right">
                     <li><a className='black-text' href="/movies">All Movies</a></li>
                     <li><a className='black-text' href="/movies/add">Add a Movie</a></li>
-                    <li><a className='black-text' href="/tvshows">TV Shows</a></li>
+                    <li><a className='black-text' href="/tvshows">All TV Shows</a></li>
                     <li><a className='black-text' href="/tvshows/add">Add a TV Show</a></li>
                 </ul>
             </div>
--- src/components/TVShowCard/TVShowCard.jsx
+++ src/components/TVShowCard/TVShowCard.jsx
@@ -4,17 +4,22 @@ import { Link } from 'react-router-dom';
 function TVShowCard(props) {
     return(
         <>
-            <div className="row">
-                <div className="col s12 m6">
-                <div className="card">
-                    <div className="card-image">
-                    <img src={props.tvshow.image ? props.tvshow.image : "https://www.cebodtelecom.com/wp-content/uploads/2014/09/related_post_no_available_image.png"} onClick={()=> {}}/>
-                    </div>
-                    <div className="card-content">
-                    <span className="card-title">{props.tvshow.name}</span>
-                    <p>{props.tvshow.description}</p>
-                    </div>
+            <div className=" card">
+                <div className="card-image waves-effect waves-block waves-light">
+                <img className="activator" src={props.tvshow.image ? props.tvshow.image : "https://www.cebodtelecom.com/wp-content/uploads/2014/09/related_post_no_available_image.png"} onClick={()=> {}}/>
                 </div>
+                <div className="card-content">
+                <span className="card-title activator grey-text text-darken-4">{props.tvshow.name}<i className="material-icons right">more_vert</i></span>
+                <p>{props.tvshow.description}</p>
+                </div>
+                <div className="card-reveal">
+                <span className="card-title grey-text text-darken-4">{props.tvshow.name}<i class="material-icons right">close</i></span>
+                <h6>IMDB Rating: {props.tvshow.imdbRating}</h6>
+                <div>Release Year:  {props.tvshow.releaseDate}</div>
+                <div>Cast: {props.tvshow.cast.join(', ')}</div>
+                <div>Seasons:  {props.tvshow.seasons}</div>
+                <div>Episodes:  {props.tvshow.episodes}</div>
+                <p>{props.tvshow.description}</p>
                 </div>
             </div>
         </>
