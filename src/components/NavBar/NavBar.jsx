4a92aa8af54b62046e76b59de5bef232fb9f2508
ManliestBen <benjamintmanley@gmail.com>
2020-07-04 21:11:10 -0500

    update styling

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -6,12 +6,12 @@ const NavBar = () => {
     <>
         <nav>
             <div className="nav-wrapper">
-                <a className=" left brand-logo" href="/"><img src="https://fontmeme.com/permalink/200704/d43fd863a6d3e281f9704c12d58a105c.png" height='66' alt="netflix-font" border="0"/></a>
+                <a className=" left brand-logo" href="/"><img src="https://fontmeme.com/permalink/200705/e2371cf438042048aa22fb9341eadbba.png" height='66' alt="netflix-font" border="0"/></a>
                 <ul id="nav-mobile" className="right">
-                    <li><a className='black-text' href="/movies">All Movies</a></li>
-                    <li><a className='black-text' href="/movies/add">Add a Movie</a></li>
-                    <li><a className='black-text' href="/tvshows">All TV Shows</a></li>
-                    <li><a className='black-text' href="/tvshows/add">Add a TV Show</a></li>
+                    <li><a href="/movies">All Movies</a></li>
+                    <li><a href="/movies/add">Add a Movie</a></li>
+                    <li><a href="/tvshows">All TV Shows</a></li>
+                    <li><a href="/tvshows/add">Add a TV Show</a></li>
                 </ul>
             </div>
         </nav>
