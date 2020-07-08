e02a25f2c58dbde624a5218b87f40ee805132258
ManliestBen <benjamintmanley@gmail.com>
2020-07-07 23:17:32 -0500

    add protected routes

--- routes/movies.js
+++ routes/movies.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
 const moviesCtrl = require('../controllers/movies');
 
-router.post('/', moviesCtrl.create);
 router.get('/', moviesCtrl.index);
+router.use(require('../config/auth'));
+router.post('/', moviesCtrl.create);
 router.delete('/:id', moviesCtrl.delete);
 router.get('/:id', moviesCtrl.show);
 router.put('/:id', moviesCtrl.update)
--- routes/tvshows.js
+++ routes/tvshows.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const tvshowsCtrl = require('../controllers/tvshows');
 
 router.get('/', tvshowsCtrl.index);
+router.use(require('../config/auth'));
 router.get('/:id', tvshowsCtrl.show);
 router.post('/', tvshowsCtrl.create);
 router.put('/:id', tvshowsCtrl.update);
