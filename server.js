85c94d95752cbcae9d5f7c5dd287eb07251751d3
ManliestBen <benjamintmanley@gmail.com>
2020-07-09 15:44:54 -0500

    add express.static

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ app.use('/api/movies', movieRouter);
 app.use('/api/tvshows', tvshowRouter);
 app.use('/api/users', userRouter);
 app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
+app.use(express.static(path.join(__dirname, 'build')));
 app.get('/*', function(req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 })
